6125cf91cdd25cb8986a89919decc37e4c9bd9ae
Cooper Stokan <cooper.stokan@dcmail.ca>
2022-01-21 09:56:50 -0500

    Updated ICE2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # WEBD6201-W2022-ICE1
 ICE 2 For WEBD6201
 Author: Cooper Stokan
-Date: January 21st, 2022
+Date: January 19th, 2022
--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,36 @@
         {
             location.href = "about.html";
         });
+
+        // Step 1. get an entry point reference (insertion point / deletion point)
+        let DocumentBody = document.body;
+        let MainContent = document.getElementsByTagName("main")[0];
+        
+        // Step 2. create an element(s) to insert (p = <p></p>)
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3. configure the new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        Article.setAttribute("class", "container");
+
+        // Step 4. Add / Insert the new element
+        // MainContent.appendChild(NewParagraph);
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        // to insert before
+        // target a location (element) to inset before
+        // TargetElement.before(elementToInsert);
+
+        // To delete
+        Article.remove();
+
+
     }
     
     function DisplayProductsPage()
